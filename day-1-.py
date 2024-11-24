908734088363b03d9923e33178c8d5fd46f93332
Tharun11102003 <tharun11102003@gmail.com>
2024-11-24 21:49:52 +0530

    changes

--- day-1-.py
+++ day-1-.py
@@ -1,4 +1,4 @@
-"""
+/*
 Day 1 Challenge: Age-Based Ticket Pricing System Problem Statement Create a system to determine the ticket price based on the visitor's age, the time of visit, and VIP membership status.
 
 Pricing Rules:
@@ -30,7 +30,7 @@ Apply Time of Day Discount: If the visit is before 11:00 AM, apply a 20% discoun
 Apply VIP Membership Discount: If the visitor is a VIP member, apply an additional 10% discount on the ticket price.
 
 This method ensures that all relevant discounts are applied correctly to calculate the final ticket price.
-"""
+*/
 
 age = int(input("Enter your age: "))
 tprice = 1000
